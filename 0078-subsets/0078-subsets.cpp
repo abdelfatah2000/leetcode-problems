95cb58a531c207b0551640a1825cf5448a368a1e
Abdelfattah Mohamed <74145788+abdelfatah2000@users.noreply.github.com>
2022-10-21 15:47:30 +0200

    Time: 4 ms (61.78%), Space: 7.4 MB (50.40%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,15 +1,30 @@
 class Solution {
+    vector<vector<int>> ans;
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        if(nums.empty()) return {{}};
-        int n = nums.back();
-        nums.pop_back();
-        vector<vector<int>> ans = subsets(nums);
-        int size = ans.size(); 
-        for(int i = 0; i < size; i++){
-            ans.push_back(ans[i]);
-            ans.back().push_back(n);
+    void backtrack(vector<int> &nums, int start, vector<int> subans){
+        ans.push_back(subans);
+        for(int i = start; i < nums.size(); i++){
+            subans.push_back(nums[i]);
+            backtrack(nums, i+1, subans);
+            subans.pop_back();
         }
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> subans;
+        backtrack(nums, 0, subans);
         return ans;
+
     }
 };
+
+
+// if(nums.empty()) return {{}};
+//         int n = nums.back();
+//         nums.pop_back();
+//         vector<vector<int>> ans = subsets(nums);
+//         int size = ans.size(); 
+//         for(int i = 0; i < size; i++){
+//             ans.push_back(ans[i]);
+//             ans.back().push_back(n);
+//         }
+//         return ans;

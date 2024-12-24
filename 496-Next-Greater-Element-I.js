1521cad2abcc0ed2e316d83c2ea2efd7edc5bce9
Abdelfattah Mohamed <74145788+abdelfatah2000@users.noreply.github.com>
2024-12-24 15:59:00 +0200

    [496] [Time Beats: 77.11%] [Memory Beats: 28.37%] - LeetPush

--- 496-Next-Greater-Element-I.js
+++ 496-Next-Greater-Element-I.js
@@ -4,19 +4,36 @@
  * @return {number[]}
  */
 var nextGreaterElement = function(nums1, nums2) {
-  let size = nums1.length;
-  let ans = new Array(size);
-  ans.fill(-1);
-  let x = 0;
-  for (let i = 0; i < size; i++) {
-    x = nums2.findIndex((val) => val === nums1[i]);
-    while (nums2[x] != undefined) {
-      if (nums1[i] < nums2[x]) {
-        ans[i] = nums2[x];
-        break;
-      }
-      x++;
+//   let size = nums1.length;
+//   let ans = new Array(size);
+//   ans.fill(-1);
+//   let x = 0;
+//   for (let i = 0; i < size; i++) {
+//     x = nums2.findIndex((val) => val === nums1[i]);
+//     while (nums2[x] != undefined) {
+//       if (nums1[i] < nums2[x]) {
+//         ans[i] = nums2[x];
+//         break;
+//       }
+//       x++;
+//     }
+//   }
+//   return ans;
+
+    let map = new Map();
+    const size = nums1.length;
+    let st = [];
+    let ans = new Array(size);
+    for (let i = 0; i < nums2.length; i++) {
+    while (st.length > 0 && st[st.length - 1] < nums2[i]) {
+        map.set(st[st.length - 1], nums2[i]);
+        st.pop();
     }
-  }
-  return ans;
+    st.push(nums2[i]);
+    }
+    for(let j = 0; j < size; j++){
+    ans[j] = map.has(nums1[j]) ? map.get(nums1[j]) : -1;
+    }
+    return ans;
+
 };
